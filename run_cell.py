26fe62ace41b828eb98711d8ddcbe7539bd8121e
vdutell <vashadutell@gmail.com>
2021-08-02 19:54:27 +0000

    python voltage experiment ball and stick

---
+++ run_cell.py
@@ -0,0 +1,146 @@
+import utils as utils
+from neuron import h
+from neuron.units import ms, mV
+import matplotlib.pyplot as plt
+
+h.load_file('stdrun.hoc')
+
+
+# load morphology and conductance properties from .hoc file
+#cell_conduct_fname = 'rgc_conduct.hoc'
+#alpha_cell_morph = h.load_file('rgc_alpha_noax.hoc')
+#beta_cell_morph = h.load_file('rgc_beta_noax.hoc')
+#cell_morph = alpha_cell_morph #beta_cell_morph
+#cell_conduct = h.load_file('rgc_conduct.hoc')
+
+# set timing properties of cell stimulation
+stim_fq_hz = 10
+stim_fq_amp = 10
+dt_ms = 25.
+dur_sec = 0.1
+timesteps = int(dur_sec * 1000. / dt_ms)
+#get time and stim vector
+time_vec, stim_vec = utils.calc_temporal_sin(stim_fq_hz, dur_sec, timesteps, stim_fq_amp)
+
+#create current clamp
+def create_stim(soma_size_multiplier=1):
+    #specify cell
+    #my_cell = utils.RGC('rgc_alpha_noax.hoc', 0)
+    my_cell = utils.BallAndStick(0, soma_size_multiplier)
+    #create current clamp - midget cell
+    stim = h.IClamp(my_cell.dend(1))
+    stim.delay = 0
+    stim.dur = 1e9
+    VecTime = h.Vector(time_vec)
+    VecStim = h.Vector(stim_vec)
+    VecStim.play(stim._ref_amp, VecTime, 1)
+
+    #record from cell
+    soma_v = h.Vector().record(my_cell.soma(0.5)._ref_v)
+    t = h.Vector().record(h._ref_t)
+
+    h.finitialize(-65 * mV)
+    h.continuerun(dur_sec * ms * 1000)
+    
+    return(t, soma_v)
+
+midget_t, midget_v = create_stim(soma_size_multiplier=1)
+midget_t_2x, midget_v_2x = create_stim(soma_size_multiplier=2)
+midget_t_10x, midget_v_10x = create_stim(soma_size_multiplier=10)
+
+
+#plot result
+plt.plot(midget_t, midget_v, label='soma 1x')
+plt.plot(midget_t_2x, midget_v_2x, label='soma 2x')
+plt.plot(midget_t_10x, midget_v_10x, label='soma 10x')
+plt.xlabel('Time (ms)')
+plt.ylabel('Soma Voltage (mV)')
+plt.title(f'Cell Response to Sine Wave at {stim_fq_hz} Hz')
+plt.legend()
+plt.savefig(f'soma_voltage_{stim_fq_hz}hz.png')
+#plt.show()
+
+#print(time_vec, stim_vec)
+
+# //Run parameters
+# dt = 0.025
+# dly = 1
+
+
+# // Patch on soma
+# objref siclamp
+# soma siclamp = new IClamp(0.5)
+# siclamp.amp = 0 // units n
+# siclamp.dur = tstop // units ms
+# siclamp.del = dly // units ms
+
+
+# //patch on dendrites
+# objref thisone, diclamp[segsize]
+# id = 0
+
+# forsec dends {
+# 	thisone = new SectionRef()
+# 	thisone diclamp[id] = new IClamp(0.5)
+# 	diclamp[id].amp = 0 // units n
+# 	diclamp[id].dur = tstop// units ms
+# 	diclamp[id].del = dly // units ms
+# 	id+=1
+# }
+
+
+# //calc sine wave stimulation as a fucntion of position and time
+# func calc_sin() {
+# 	xpos_um = $1
+# 	time_ms = $2
+# 	xpos_mm = xpos_um*1000
+
+# 	temp_fq_ms = temp_fq_sec/1000 //convert temp fq from hz to cyc per ms
+# 	cat_mmpdeg = 0.218 // vis_ang for cat: 1 deg = 0.218mm (Visual Perception: The Neurophysiological Foundations)
+# 	spac_fq_mm = spac_fq_deg/cat_mmpdeg //convert spat fq from cpd to cyc per mm
+
+# 	fx = xpos_mm*spac_fq_mm //spatial frequency component
+# 	wt = time_ms*temp_fq_ms //temporal frequency component
+# 	res = sin(2*PI*(fx-wt)) //calculate sine wave
+	
+# 	return(res)
+# }
+
+
+# //proc init(){
+# //	Iin = 0
+# //}
+
+# proc advance() {
+#  	fadvance()
+#  	time_delayed = t-siclamp.del
+
+#  	dends{ //for i=0,n3d()-1{
+#  		i=0
+#  		xcoord = x3d(i)
+#  		sine_val = calc_sin(xcoord,t)
+#  		dend[i] diclamp.amp = ampl+ampl*sine_val
+ 		
+#  	}	
+#  	Iin = ampl+ampl*sine_val
+# }
+
+# //init()
+
+# proc pp() {
+# 	spac_fq_deg = $1 //fq in cpd
+# 	temp_fq_sec = $2 //fq in hz
+# 	ampl = $3 //amplitude
+# 	soma_multiplier = $4 //soma size multiplier
+
+# 	soma_original = soma.diam
+# 	soma.diam = soma_original*soma_multiplier
+
+# 	init()
+# 	advance()
+# 	run()
+
+# 	soma.diam = soma_original
+
+# }
+
---
+++ utils.py
@@ -0,0 +1,84 @@
+import numpy as np
+from neuron import h
+from neuron.units import ms, mV
+
+def calc_temporal_sin(temp_fq_hz, len_s, n_timepoints, amplitude):
+    '''
+    Function to calculate a temporal sine wave for neuron stimluation
+
+    Input:
+        temp_fq_hz (int): Temporal frequency at which to modulate sime wave
+        len_s (int): Length of stimulation in seconds
+        n_timepoints (int): Number of timepoints
+        amplitude (int): Amplitude of stimulation
+    Returns:
+        timepoints (1D numpy array): Timepoint vector
+        sin_stim_vec (1D numpy array, float32): Sine wave stim vector of stim values
+    '''
+
+    timepoints = np.linspace(0, len_s, n_timepoints)
+    sin_stim_vec = np.sin(2*np.pi*(temp_fq_hz * timepoints)) #calculate sine wave
+    return(timepoints, sin_stim_vec)
+
+# h.load_file('rgc_alpha_noax.hoc')
+
+# #specify cell from morphology and conductance files
+# class RGC:
+#     def __init__(self, morph_fname, gid):
+#         self._gid = gid
+#         self.morph_fname = morph_fname
+#         self._setup_morphology()
+#         self._setup_biophysics()
+#     def _setup_morphology(self):
+#         self.soma = soma #morph_params.soma
+#         #self.initseg = morph_params.initset
+#         #self.narrowr = morph_params.narrowr
+#         #self.soma = h.Section(name='soma', cell=self)
+#         #self.dend = h.Section(name='dend', cell=self)
+#         self.all = [self.soma, self.initseg, self.narrowr]
+#     def _setup_biophysics(self):
+        
+#         for seg in self.soma:
+#             seg.hh.gnabar = 0.12  # Sodium conductance in S/cm2
+#             seg.hh.gkbar = 0.036  # Potassium conductance in S/cm2
+#             seg.hh.gl = 0.0003    # Leak conductance in S/cm2
+#             seg.hh.el = -54.3     # Reversal potential in mV
+#         # Insert passive current in the dendrite                       # <-- NEW
+#         self.dend.insert('pas')
+#         for seg in self.dend:                                          # <-- NEW
+#             seg.pas.g = 0.001  # Passive conductance in S/cm2          # <-- NEW
+#             seg.pas.e = -65    # Leak reversal potential mV            # <-- NEW
+#     def __repr__(self):
+#         return 'BallAndStick[{}]'.format(self._gid)
+
+class BallAndStick:
+    def __init__(self, gid, soma_size_multiplier):
+        self._gid = gid
+        self.soma_size_multiplier = soma_size_multiplier
+        self._setup_morphology()
+        self._setup_biophysics()
+    def _setup_morphology(self):
+        self.soma = h.Section(name='soma', cell=self)
+        self.dend = h.Section(name='dend', cell=self)
+        self.all = [self.soma, self.dend]
+        self.dend.connect(self.soma)
+        self.soma.L = self.soma.diam = 200 * self.soma_size_multiplier #12.6157
+        self.dend.L = 200
+        self.dend.diam = 1
+    def _setup_biophysics(self):
+        for sec in self.all:
+            sec.Ra = 100    # Axial resistance in Ohm * cm
+            sec.cm = 1      # Membrane capacitance in micro Farads / cm^2
+        self.soma.insert('hh')
+        for seg in self.soma:
+            seg.hh.gnabar = 0.12  # Sodium conductance in S/cm2
+            seg.hh.gkbar = 0.036  # Potassium conductance in S/cm2
+            seg.hh.gl = 0.0003    # Leak conductance in S/cm2
+            seg.hh.el = -54.3     # Reversal potential in mV
+        # Insert passive current in the dendrite                       # <-- NEW
+        self.dend.insert('pas')                                        # <-- NEW
+        for seg in self.dend:                                          # <-- NEW
+            seg.pas.g = 0.001  # Passive conductance in S/cm2          # <-- NEW
+            seg.pas.e = -65    # Leak reversal potential mV            # <-- NEW
+    def __repr__(self):
+        return 'BallAndStick[{}]'.format(self._gid)
